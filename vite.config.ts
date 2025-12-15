40811a98974a408302d7ada41b3d1abcfa004ecf
xiayuguo <xiayuguo@wisdomgarden.com>
2025-12-15 12:39:12 +0800

    refactor: add generate og

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { Buffer } from 'node:buffer'
-import { basename, dirname, resolve } from 'node:path'
+// import { Buffer } from 'node:buffer'
+import { basename, resolve } from 'node:path'
 import MarkdownItShiki from '@shikijs/markdown-it'
 import { transformerNotationDiff, transformerNotationHighlight, transformerNotationWordHighlight } from '@shikijs/transformers'
 import { rendererRich, transformerTwoslash } from '@shikijs/twoslash'
@@ -132,13 +132,9 @@ export default defineConfig({
           const route = basename(id, '.md')
           if (route === 'index' || frontmatter.image || !frontmatter.title)
             return
-          const path = `og/${route}.png`
-          promises.push(
-            fs.existsSync(`${id.slice(0, -3)}.png`)
-              ? fs.copy(`${id.slice(0, -3)}.png`, `public/${path}`)
-              : generateOg(frontmatter.title!.replace(/\s-\s.*$/, '').trim(), `public/${path}`),
-          )
-          frontmatter.image = `https://yuguo.im/${path}`
+          if (!frontmatter.image && frontmatter.title) {
+            frontmatter.image = `https://yuguo.im/og/${route}.png`
+          }
         })()
         const head = defaults(frontmatter, options)
         return { head, frontmatter }
@@ -198,33 +194,33 @@ export default defineConfig({
   },
 })
 
-const ogSVg = fs.readFileSync('./scripts/og-template.svg', 'utf-8')
+// const ogSVg = fs.readFileSync('./scripts/og-template.svg', 'utf-8')
 
-async function generateOg(title: string, output: string) {
-  if (fs.existsSync(output))
-    return
+// async function generateOg(title: string, output: string) {
+//   if (fs.existsSync(output))
+//     return
 
-  const { default: sharp } = await import('sharp')
-  await fs.mkdir(dirname(output), { recursive: true })
-  // breakline every 30 chars
-  const lines = title.trim().split(/(.{0,30})(?:\s|$)/g).filter(Boolean)
+//   const { default: sharp } = await import('sharp')
+//   await fs.mkdir(dirname(output), { recursive: true })
+//   // breakline every 30 chars
+//   const lines = title.trim().split(/(.{0,30})(?:\s|$)/g).filter(Boolean)
 
-  const data: Record<string, string> = {
-    line1: lines[0],
-    line2: lines[1],
-    line3: lines[2],
-  }
-  console.warn('Generating og image data:', data)
-  const svg = ogSVg.replace(/\{\{([^}]+)\}\}/g, (_, name) => data[name] || '')
+//   const data: Record<string, string> = {
+//     line1: lines[0],
+//     line2: lines[1],
+//     line3: lines[2],
+//   }
+//   console.warn('Generating og image data:', data)
+//   const svg = ogSVg.replace(/\{\{([^}]+)\}\}/g, (_, name) => data[name] || '')
 
-  console.log(`Generating ${output}`)
-  try {
-    await sharp(Buffer.from(svg))
-      .resize(1200 * 1.1, 630 * 1.1)
-      .png()
-      .toFile(output)
-  }
-  catch (e) {
-    console.error('Failed to generate og image', e)
-  }
-}
+//   console.log(`Generating ${output}`)
+//   try {
+//     await sharp(Buffer.from(svg))
+//       .resize(1200 * 1.1, 630 * 1.1)
+//       .png()
+//       .toFile(output)
+//   }
+//   catch (e) {
+//     console.error('Failed to generate og image', e)
+//   }
+// }
